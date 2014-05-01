5121f5f25f323f7ae73d4ed9d7d53a6ea82f4acb
cesar_ciau <cesarfhs@gmail.com>
2014-05-01 12:50:28 -0500

    Segundo commit
    saltos cambiados

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'wvniR93b0qyUubWiIx2G0DYhGFJfgaC9mfs2guVo');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '85967496836454535430965776492');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
